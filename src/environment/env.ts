8c266dfa7d60b36004d88460d9503f8b8fc92d9d
Dennis <dennisjcarvalho@gmail.com>
2023-04-28 14:35:44 -0300

    ajustando environment

--- src/environment/env.ts
+++
@@ -1,8 +0,0 @@
-export const env = {
-  production: false,
-  MONGODB_URI: 'mongodb+srv://dennisjcarvalho:nCkv8z2b201YBUuB@cluster0.kelzjza.mongodb.net/test',
-  MONGODB_LOCAL: 'mongodb://localhost:27017/mystudies'
-}
-
-
-
