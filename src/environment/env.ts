2fdd9150289d2b99831efc04e33c477f7f40fb39
Dennis <dennisjcarvalho@gmail.com>
2023-04-28 13:53:58 -0300

    liberando acesso ao render

---
+++ src/environment/env.ts
@@ -0,0 +1,8 @@
+export const env = {
+  production: false,
+  MONGODB_URI: 'mongodb+srv://dennisjcarvalho:nCkv8z2b201YBUuB@cluster0.kelzjza.mongodb.net/test',
+  MONGODB_LOCAL: 'mongodb://localhost:27017/mystudies'
+}
+
+
+
