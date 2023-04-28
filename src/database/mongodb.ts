11dcaefc2ef3ce9efcdea88588e004a91bb9a995
Dennis <dennisjcarvalho@gmail.com>
2023-04-28 11:06:38 -0300

    config: stream de configuracao para o mongo db, variaveis de ambiente

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -1,7 +1,11 @@
 import { connect } from 'mongoose';
+import { Enviroment } from '../environment/keys';
 
+
+//mongodb://localhost:27017/mystudies
 export function connectDatabase() {
-  connect('mongodb://localhost:27017/mystudies')
-    .then(res => console.log(`Mongodb connected.`))
+  connect(Enviroment.STREAM_MONGODB)
+    .then(res => console.log(`Mongodb connected in cloud.`))
     .catch(error => console.error(`No posible connect ${error}`));
 }
+
